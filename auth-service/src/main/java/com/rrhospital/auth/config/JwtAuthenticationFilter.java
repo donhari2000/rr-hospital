19a1a61ba75ffebc53a366524163f2a360df1cbd
Hariharan Sekar <hariharan.a.sekar@accenture.com>
2026-07-23 18:57:04 +0530

    fatel: Day 11 created CustomUserDetails to get user role

--- auth-service/src/main/java/com/rrhospital/auth/config/JwtAuthenticationFilter.java
+++ auth-service/src/main/java/com/rrhospital/auth/config/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.rrhospital.auth.config;
 
+import com.rrhospital.auth.security.CustomUserDetailsService;
 import com.rrhospital.auth.utils.JwtUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -9,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.context.SecurityContextHolderFilter;
 import org.springframework.stereotype.Component;
@@ -22,6 +24,8 @@ import java.util.List;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
+    private final CustomUserDetailsService customUserDetailsService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
@@ -39,9 +43,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             String token = authHeader.substring(7);
             String email = jwtUtil.extractEmail(token);
+            UserDetails userDetails = customUserDetailsService.loadUserByUsername(email);
             if(jwtUtil.validateToken(token, email)){
-                var auth = new UsernamePasswordAuthenticationToken(email, null, List.of());
-                SecurityContextHolder.getContext().setAuthentication(auth);
+//                var auth = new UsernamePasswordAuthenticationToken(email, null, List.of());
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                SecurityContextHolder.getContext().setAuthentication(authentication);
             }
         }
         filterChain.doFilter(request,response);
